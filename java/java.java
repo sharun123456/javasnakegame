b1a8e8cb347d97569cfdde955a13dc5d08afb95e
sharun kanisetty <kanisettysharun@gmail.com>
2024-01-30 01:28:45 +0530

    java.txt modified

---
+++ java/java.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+public class snake
+{
+    public static void main(String[] args)
+    {
+        int boardwidth = 600;
+        int boardheight = boardwidth;
+
+        JFrame frame = new JFrame("snake");
+        frame.setVisible(true);
+        frame.setSize(boardwidth,boardheight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+
+
+
+    }
+}
